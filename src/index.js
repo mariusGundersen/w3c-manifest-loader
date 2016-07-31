a3163e05d8139eba76cf92dd522ad04614a01fcf
Marius Gundersen <me@mariusgundersen.net>
2016-07-31 15:52:29 +0200

    wip, initial code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,58 @@
-export default function w3cManifest(source){
+import "babel-polyfill";
+
+import {createReadStream} from 'fs';
+import glob from 'glob';
+import {getLoaderConfig} from 'loader-utils';
+import path from 'path';
+import probeSize from 'probe-image-size';
+
+export default async function w3cManifest(source){
   this.cacheable();
   const callback = this.async();
 
-  callback(null, source);
+  try{
+    const options = getLoaderConfig(this, 'w3c-manifest');
+
+    const manifest = JSON.parse(source);
+    const icons = getIconsArray(manifest);
+
+    const listOfGlobResults = await Promise.all(icons.map(icon => globAsPromised(icon, {})));
+    const listOfIcons = await Promise.all(listOfGlobResults.map(probeSizeAsPromised));
+
+    const iconData = lists.reduce((a, b) => ([...a, ...b]), []);
+
+    manifest.icons = iconData.map(({path, width, height, mime}) => ({
+      src: path,
+      size: `${width}x${height}`,
+      type: mime
+    }));
+
+    const result = JSON.stringify(manifest, null, '  ');
+    callback(null, result);
+  }catch(err){
+    callback(err);
+  }
+}
+
+function getIconsArray(manifest){
+  const icons = 'icons' in manifest ? manifest.icons : [];
+
+  return Array.isArray(icons) ? icons : [icons];
+}
+
+function globAsPromised(pattern, options){
+  return new Promise((resolve, reject) =>
+    glob(pattern, options,
+      (err, files) => err ? reject(err) : resolve(filse)));
+}
+
+function probeSizeAsPromised(path){
+  const input = createReadStream(icon);
+  return new Promise(function(resolve, reject){
+    probeSize(path, (err, result) => {
+      input.destroy();
+
+      return err ? reject(err) : resolve({path:path, ...result});
+    });
+  });
 }
