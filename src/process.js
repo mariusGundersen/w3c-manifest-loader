d635745b44f2cd75519c936d7fde0e8578a2f7ea
Marius Gundersen <me@mariusgundersen.net>
2016-08-01 01:06:02 +0200

    produce legacy Apple output too
    from https://developer.apple.com/library/ios/documentation/AppleApplications/Reference/SafariWebContent/ConfiguringWebApplications/ConfiguringWebApplications.html

--- src/process.js
+++ src/process.js
@@ -2,25 +2,34 @@ import {interpolateName} from 'loader-utils';
 import {join} from 'path';
 import u from 'untab';
 
-import parseIcons from './parseIcons.js';
-
 export default async function process(manifest, context, config){
-    const resultManifest = await parseIcons(manifest, config);
+  const content = JSON.stringify(manifest, null, '  ');
+  const url = interpolateName(context, config.name, {
+      context: config.context || context.options.context,
+      content: content,
+      regExp: config.regExp
+    });
 
-    const content = JSON.stringify(resultManifest, null, '  ');
-    const url = interpolateName(context, config.name, {
-        context: config.context || context.options.context,
-        content: content,
-        regExp: config.regExp
-      });
+  const publicPath = "__webpack_public_path__ + " + JSON.stringify(url);
+  context.emitFile(url, content);
 
-    const publicPath = "__webpack_public_path__ + " + JSON.stringify(url);
-    context.emitFile(url, content);
-
-    return u`
+  return u`
+    (function(){
       var link = document.createElement('link');
       link.setAttribute('rel', 'manifest');
       link.setAttribute('href', ${publicPath});
       document.head.appendChild(link);
-    `;
+    })();
+    ${manifest.icons.map(legacyAppleIcon).join('')}`;
+}
+
+function legacyAppleIcon(icon){
+  return u`
+    (function(){
+      var link = document.createElement('link');
+      link.setAttribute('rel', 'apple-touch-icon');
+      link.setAttribute('sizes', '${icon.sizes}');
+      link.setAttribute('href', '${icon.src}');
+      document.head.appendChild(link);
+    })();`;
 }
