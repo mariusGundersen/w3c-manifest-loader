b720ee95f3d14e5d160d295717f3456167e043a8
Marius Gundersen <me@mariusgundersen.net>
2016-08-01 16:51:56 +0200

    More legacy apple support

--- src/process.js
+++ src/process.js
@@ -14,22 +14,36 @@ export default async function process(manifest, context, config){
   context.emitFile(url, content);
 
   return u`
-    (function(){
-      var link = document.createElement('link');
-      link.setAttribute('rel', 'manifest');
-      link.setAttribute('href', ${publicPath});
-      document.head.appendChild(link);
-    })();
-    ${(manifest.icons || []).map(legacyAppleIcon).join('')}`;
+    var link = document.createElement('link');
+    link.setAttribute('rel', 'manifest');
+    link.setAttribute('href', ${publicPath});
+    document.head.appendChild(link);
+    ${config.legacyAppleSupport ? legacyAppleManifest(manifest) : ''}`;
 }
 
-function legacyAppleIcon(icon){
+function legacyAppleManifest(manifest){
   return u`
-    (function(){
-      var link = document.createElement('link');
-      link.setAttribute('rel', 'apple-touch-icon');
-      link.setAttribute('sizes', '${icon.sizes}');
-      link.setAttribute('href', '${icon.src}');
-      document.head.appendChild(link);
-    })();`;
+    var manifest = ${JSON.stringify(manifest)};
+    if(manifest.icons && manifest.icons.length){
+      for(var i in manifest.icons){
+        var link = document.createElement('link');
+        link.setAttribute('rel', 'apple-touch-icon');
+        link.setAttribute('sizes', manifest.icons[i].sizes);
+        link.setAttribute('href', manifest.icons[i].src);
+        document.head.appendChild(link);
+      }
+    }
+    if(manifest.display && (manifest.display == 'fullscreen' || manifest.display == 'standalone')){
+      var meta = document.createElement('meta');
+      meta.setAttribute('rel', 'apple-mobile-web-app-capable');
+      meta.setAttribute('content', 'yes');
+      document.head.appendChild(meta);
+    }
+    if(manifest.theme_color){
+      var meta = document.createElement('meta');
+      meta.setAttribute('rel', 'apple-mobile-web-app-status-bar-style');
+      meta.setAttribute('content', manifest.theme_color);
+      document.head.appendChild(meta);
+    }
+  `;
 }
